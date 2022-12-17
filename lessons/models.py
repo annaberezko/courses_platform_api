e488254621aa78b1a032c8c46560922270b287f2
annaberezko <site.vizit@gmail.com>
2022-12-17 21:43:48 +0200

    Initial migration in lessons

--- lessons/models.py
+++ lessons/models.py
@@ -17,7 +17,6 @@ class Lesson(models.Model):
     video = models.CharField('video link', max_length=100, null=True, blank=True)
     text = models.TextField('text', null=True, blank=True)
     home_task = models.TextField(null=True, blank=True)
-    tests_task = models.BooleanField(default=False)
 
 
 class Material(models.Model):
@@ -26,14 +25,3 @@ class Material(models.Model):
 
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, related_name='materials')
     file = models.FileField(upload_to=file_path, validators=[FileExtensionValidator(FILES_EXTENSIONS), validate_size])
-
-
-class Test(models.Model):
-    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
-    question = models.CharField('lesson name', max_length=120)
-
-
-class Option(models.Model):
-    test = models.ForeignKey(Test, on_delete=models.CASCADE)
-    option = models.CharField('test option', max_length=100)
-    correct = models.BooleanField(default=False)
